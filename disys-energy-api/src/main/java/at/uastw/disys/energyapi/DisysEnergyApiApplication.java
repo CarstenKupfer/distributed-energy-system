52e93494fba5db2e0d2e33e62b031d3b9be8ae72
CarstenKupfer <wi24b087@technikum-wien.at>
2026-05-03 18:59:52 +0200

    Initial project setup with REST API

---
+++ disys-energy-api/src/main/java/at/uastw/disys/energyapi/DisysEnergyApiApplication.java
@@ -0,0 +1,13 @@
+package at.uastw.disys.energyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisysEnergyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisysEnergyApiApplication.class, args);
+    }
+
+}
---
+++ disys-energy-api/src/test/java/at/uastw/disys/energyapi/DisysEnergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.uastw.disys.energyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisysEnergyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
