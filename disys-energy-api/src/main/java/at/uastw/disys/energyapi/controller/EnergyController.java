a573b3ec4bac69091868d904effe9ae440e634d1
m1key1234 <michael.reithofer1@gmail.com>
2026-05-03 21:21:38 +0200

    Add current energy endpoint

--- disys-energy-api/src/main/java/at/uastw/disys/energyapi/controller/EnergyController.java
+++ disys-energy-api/src/main/java/at/uastw/disys/energyapi/controller/EnergyController.java
@@ -1,4 +1,17 @@
 package at.uastw.disys.energyapi.controller;
 
+import at.uastw.disys.energyapi.dto.CurrentPercentageDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EnergyController {
+
+    @GetMapping("/energy/current")
+    public CurrentPercentageDto getCurrentEnergy() {
+        return new CurrentPercentageDto(
+                100.0,
+                5.63
+        );
+    }
 }
